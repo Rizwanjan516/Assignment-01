6e072dfc082e6671aef2096eea9ddb458e6e3604
Rizwanjan516 <Rizwan.jan2009@gmail.com>
2024-10-30 18:12:12 +0500

    Add files via upload

---
+++ first.py
@@ -0,0 +1,36 @@
+num1=10
+num2=20
+num3=30
+print(num1+num2+num3)# for sum
+num1=10
+num2=20
+num3=30
+print(num1-num2-num3)#for difference
+num1=10
+num2=20
+num3=30
+print(num1*num2*num3)#for product
+num1=60
+num2=-40
+num3-6000
+print(60+-40+6000)#for avereage
+num=6020
+print(6020/3)
+
+"triangle"
+base=30
+height=50
+area=print(0.5*height*base) # for triangle area
+
+"rectangle"
+length=35
+width=20
+area=print(length*width) # for rectangle area
+
+"square"
+side=20
+area=print(side**2) # for square area
+
+"circle"
+radius=6
+area=print(3.14159*(radius**2)) # for circle
